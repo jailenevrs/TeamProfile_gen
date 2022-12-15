512f821cccf883bb0b3aa22d2b0ff2125366992d
Jailene <jaileneveras8@gmail.com>
2022-12-14 22:31:19 -0500

    imported classes to index.js

--- html_template.js
+++ html_template.js
@@ -12,7 +12,7 @@ const generateHTML = function() {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>`
-}
+
 
 
 
@@ -57,7 +57,8 @@ const generateIntern = function (Intern) {
   </div>
 </div>`
 }
+}
 
 
 //Export to index
-module.exports = {generateEnginer,generateIntern,generateManager,generateHTML};
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const html = require("./html_template");
+const template = require("./html_template");
+const { inherits } = require("util");
+const e = require("express");
+const Manager = require("./libs/manager");
+const Engineer = require("./libs/engineer");
+const Intern = require("./libs/intern");
+
+//array of all employees
+const employeeArray = []
+
 
 //adding manager 
 function askManager() { 
@@ -29,7 +38,8 @@ function askManager() {
 
     }
 ]).then ((answers)=>{
-console.log(answers)
+    const managerAnswers = new Manager (answers.Name,answers.ID,answers.Office_number,answers.email)
+    employeeArray.push(managerAnswers)
 TeamMenu();
 })
 
@@ -39,7 +49,7 @@ function askEngineer(){
     inquirer.prompt(
         [
             {
-                name:"Name",
+                name:"name",
                 message:"What is the employee's name",
                 type:"input"
             },
@@ -61,11 +71,12 @@ function askEngineer(){
             }
         ]
     ).then((answers)=>{
-        console.log(answers)
+        const EngineerAnswers = new Engineer (answers.name,answers.ID,answers.email,answers.githubUser)
+        employeeArray.push(EngineerAnswers)
         TeamMenu();
     })
 }
-function AskIntern(){
+function askIntern(){
     inquirer.prompt([
     {
         name:"Name",
@@ -81,7 +92,7 @@ function AskIntern(){
         {
             name:"email",
             message:"what is the employee's email?",
-            type:"text"
+            type:"input"
 
     },
     {
@@ -92,6 +103,8 @@ function AskIntern(){
     }]
     ) .then ((answers)=>{
         console.log(answers)
+        const InternAnswers = new Intern (answers.name,answers.ID,answers.email,answers.school)
+        employeeArray.push(InternAnswers)
         TeamMenu();
     }
     )
@@ -112,67 +125,66 @@ if(answers.TeamMem==="Engineer")
     } 
     if (answers.TeamMem==="Intern"){
         askIntern()
+    } 
+    else {
+        console.log("Your team is complete!")
+
+        generateHTML();
     }
     })
 }
-    function addEmployee(){ inquirer.prompt([
-        {
-        name:"TeamMem",
-        message:"Would you like to add another Team member?",
-        type:"list",
-        choices:['Engineer','Intern','Im done building team']
-        },
-        {
-            name:"Name",
-            message:"What is the employee's name",
-            type:"text"
-        },
-        { 
-            name:"ID",
-            message: "what is the employee's id?",
-            type:"text"
+    // function addEmployee(){ inquirer.prompt([
+    //     {
+    //     name:"TeamMem",
+    //     message:"Would you like to add another Team member?",
+    //     type:"list",
+    //     choices:['Engineer','Intern','Im done building team']
+    //     },
+    //     {
+    //         name:"Name",
+    //         message:"What is the employee's name",
+    //         type:"input"
+    //     },
+    //     { 
+    //         name:"ID",
+    //         message: "what is the employee's id?",
+    //         type:"input"
 
-        },
-        {
-            name:"email",
-            message:"what is the employee's email?",
-            type:"text"
-        },
-        {
-            name:"Github",
-            message:"What is employee's github username?",
-            type:"text",
-            when: (input) => input.TeamMem === "Engineer"
+    //     },
+    //     {
+    //         name:"email",
+    //         message:"what is the employee's email?",
+    //         type:"input"
+    //     },
+    //     {
+    //         name:"Github",
+    //         message:"What is employee's github username?",
+    //         type:"input",
+    //         when: (input) => input.TeamMem === "Engineer"
 
-        },
-          {
-         name:"school",
-         message:"What school does the intern attend?",
-         type:"text",
-         when: (input)=> input.TeamMem === "Intern"
-          }
+    //     },
+    //       {
+    //      name:"school",
+    //      message:"What school does the intern attend?",
+    //      type:"input",
+    //      when: (input)=> input.TeamMem === "Intern"
+    //       }
 
-    ])
-    .then ((answers) =>{
-        console.log(answers)
-    })
-    }
+   // ])
+    // .then ((answers) =>{
+    //     console.log(answers)
+    // })
+//}
 
-    //
+    
     
     //writing index.html
 
-    //function init() {
-      //  inquirer.prompt(questions)
-        //.then(response=>{
-          //  fs.writeFile("index.html",(response),
-            //error=>{
-              //  if(error){
-                //    console.log(error);
-                //} else (console.log("response saved succesfully!"))
-            //});
-        //});
+    function generateHTML(team) {
+           //fs.writeFile("index.html",(template(team)),"utf-8")
+        console.log("succesfully wrote to html!!")
+        };
 
-        //init();
+    
  
 askManager();
